29cd5d27b503a2420b4eacb9d7a80a5202d38021
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-10 20:07:38 +0100

    finilized the REST API's

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const PORT = 4000;
 const User = require("./models").user;
 const app = express();
+const TodoList = require("./models").todoList;
 
 //body parser
 app.use(express.json());
@@ -49,5 +50,119 @@ app.post("/users", async (req, res, next) => {
 //  const users = await User.findAll();
 //  res.send(users);
 //});
+
+//get a user's information
+
+app.get("/users/:userId", async (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const user = await User.findByPk(userId);
+  if (!user) {
+    res.status(404).send("User not found");
+  } else {
+    res.send(user);
+  }
+});
+
+//update user's information
+
+app.put("/users/:userId", async (req, res, next) => {
+  try {
+    const userId = parseInt(req.params.userId);
+    console.log("user id: ", userId);
+    const userToUpdate = await User.findByPk(userId);
+    console.log("user to update ", userToUpdate);
+    if (!userToUpdate) {
+      res.status(404).send("User not found");
+    } else {
+      const updatedUser = await userToUpdate.update(req.body);
+      res.json(updatedUser);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+//get all user's tasks
+
+app.get("/users/:userId/lists", async (req, res, next) => {
+  try {
+    const userId = parseInt(req.params.userId);
+    const user = await User.findByPk(userId, {
+      include: [TodoList],
+    });
+    if (user) {
+      res.send(user.TodoLists);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+//create a new task
+
+app.post("/users/:userId/lists", async (req, res, next) => {
+  try {
+    const userId = parseInt(req.params.userId);
+    const user = await User.findByPk(userId);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      const newList = await TodoList.create({ userId, ...req.body });
+      res.json(newList);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+// Update an existing list
+app.put("/users/:userId/lists/:listId", async (req, res, next) => {
+  try {
+    const listId = parseInt(req.params.listId);
+    const toUpdate = await TodoList.findByPk(listId);
+    if (!toUpdate) {
+      res.status(404).send("List not found");
+    } else {
+      const updated = await toUpdate.update(req.body);
+      res.json(updated);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+// Delete a user's list
+app.delete("/users/:userId/lists/:listId", async (req, res, next) => {
+  try {
+    const listId = parseInt(req.params.listId);
+    const toDelete = await TodoList.findByPk(listId);
+    if (!toDelete) {
+      res.status(404).send("List not found");
+    } else {
+      const deleted = await toDelete.destroy();
+      res.json(deleted);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+// Delete all user's lists
+app.delete("/users/:userId/lists", async (req, res, next) => {
+  try {
+    const userId = parseInt(req.params.userId);
+    const user = await User.findByPk(userId, { include: [TodoList] });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      user.TodoLists.forEach(async (list) => await list.destroy());
+      res.status(204).send();
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
 app.listen(PORT, () => console.log(`Server started in port: ${PORT}`));
