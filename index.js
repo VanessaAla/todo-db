c67a080be5b8b1d7e32ad69d919359e95575aa52
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-10 15:22:48 +0100

    created another user

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const PORT = 4000;
 const User = require("./models").user;
 const app = express();
 
+//body parser
 app.use(express.json());
 
 app.get("/test", (req, res) => {
@@ -14,7 +15,7 @@ app.post("/echo", (req, res) => {
   res.json(req.body);
 });
 
-// Create a new user account
+// Create a new user account {by email, name and password}
 app.post("/users", async (req, res, next) => {
   try {
     const { email, name, password } = req.body;
@@ -23,6 +24,21 @@ app.post("/users", async (req, res, next) => {
     } else {
       const newUser = await User.create({ email, name, password });
       res.send(newUser);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+//Create a new user {only by email}
+app.post("/users", async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    if (!email || email === " ") {
+      res.status(400).send("Must provide an email address");
+    } else {
+      const user = await User.create(req.body);
+      res.json(user);
     }
   } catch (e) {
     next(e);
